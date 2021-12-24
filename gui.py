54db1f8cc81a87e8256ee7739fda454338cb6cd4
Andrew Flores <andrewtflores@yahoo.com>
2021-12-24 14:33:00 -0600

    Move classes to own files and changed removeprefix

--- gui.py
+++ gui.py
@@ -7,106 +7,10 @@
 
 import re
 import time
-# import pandas as pd
-import json
-import time
-import string
-from collections import Counter
-
 from tkinter import *
-from PIL import ImageTk, Image
+from autocorrect import SpellChecker
+from generator import TextGeneration
 
-
-class SpellChecker(object):
-  def __init__(self, wordlist_path, probabilities_path):
-    import json
-    # Import probabilities
-    with open(probabilities_path, 'r') as inFile:
-      self.probs = json.load(inFile)
-
-    # Import set of all english words
-    import ast
-    with open(wordlist_path,'r') as f:
-      self.vocab = set(ast.literal_eval(f.read()))
-
-  def _level_one_edits(self, word): # Words misspelled by one letter
-    letters = string.ascii_lowercase + "\'" # List of all 26 letters
-    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)] # Find all ways to split a word [(w, ord), (wo, rd), (wor, d)]
-    deletes = [l + r[1:] for l, r in splits if r] # Find all ways of having a missing letter [ord, wrd, wod, wor]
-    swaps = [l + r[1] + r[0] + r[2:] for l,r in splits if len(r) > 1] # Find all ways of swapping letters [owrd, wrod, wodr]
-    replaces = [l + c + r[1:] for l,r in splits if r for c in letters] # Find all ways of replacing a letter [aord, bord,..., worz]
-    inserts = [l + c + r for l,r in splits for c in letters] # Find all ways of inserting a letter [aword, bword,..., wordz]
-    return set(deletes + swaps + replaces + inserts) # Return set of all unique possibilities
-
-  def _level_two_edits(self, word): # Words misspelled by two letters
-    return set([e2 for e1 in self._level_one_edits(word) for e2 in self._level_one_edits(e1)]) # Run level_one_edits twice to get words two letters off
-
-  def _level_three_edits(self, word): # Words misspelled by three letters
-    return set([e2 for e1 in self._level_one_edits(word) for e2 in self._level_two_edits(e1)]) # Run L1edits for each L2edit to get words 3 letters off
-
-  def check(self, word): # Spellcheck algorithm
-    present = spelled_correctly(word)
-    if present:
-      return 'Spelled correctly.'
-
-    candidates = self._level_one_edits(word) or self._level_two_edits(word) or self._level_three_edits(word) or [word] # Gather all possible mispellings
-    best_guesses = [w for w in candidates if w in self.vocab] # Only suggest spellings that are real words
-    sorted_tuples = sorted([(w, self.probs[w]) for w in best_guesses], key = lambda x: x[1], reverse=True) # Sort possible words
-
-    sum_probs = 0
-    for e in sorted_tuples:
-      sum_probs += e[1]
-    confidence_tuples = []
-    for e in range(len(sorted_tuples)):
-       c = 100 * sorted_tuples[e][1] / sum_probs # Calculate and store confidence
-       w = sorted_tuples[e][0] # Store word
-       t = (w, c)# Create tuple
-       confidence_tuples.append(t) # Add (word, confidence) to result
-    if present:
-      return "Spelled Correctly."
-    else:
-      return confidence_tuples[:3]
-
-  def get_vocab(self):
-    return self.vocab
-
-class TextGeneration(object):
-  n_dict = {}
-  def __init__(self, ngrams_path):
-      with open(ngrams_path, 'r') as inFile:
-          self.n_dict = json.load(inFile)
-
-  # Joins list of words into string
-  def join_words(self, l):
-      result = ''
-      for w in l:
-          result += w + ' '
-      # Return string without trailing spaces
-      return result.strip()
-
-  # Find best match first (longest seed match with length >= words.len() + 1)
-  # Gets last 3 words of string, or two or one
-  def get_last_three(self, str, seed):
-      words = str.strip(' ,.()\'\"').split(' ')
-      subset = []
-      suggestions = []
-      # Get 3 suggestions, or stop if looking if no matches
-      while len(suggestions) < 3 and seed > 0:
-          subset = words[-seed:]
-          subset_str = self.join_words(subset)
-          for key in self.n_dict:
-              if len(suggestions) == 3:
-                  break
-              if self.n_dict[key] == seed + 1:
-                  # if subset is found in the FIRST part of the key
-                  if re.search(r'\b' + subset_str + r'\b', key) and key.find(subset_str) == 0:
-                      suggestions.append(key.removeprefix(subset_str).strip())
-          seed -= 1
-      # Suggestions are formed with original prompt still included
-      pared_suggestions = []
-      for e in suggestions:
-          pared_suggestions.append(e.removeprefix(subset_str).strip())
-      return pared_suggestions
 
 # Flush outputs
 def flush(event):
